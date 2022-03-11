0fd84346352476b5790fca6e9f32f6152590c60d
Jeong Seok <wjdtjr8649@naver.com>
2022-03-11 23:58:51 +0900

    5th commit

--- react-foundation-youtube/src/component/DayList.js
+++ react-foundation-youtube/src/component/DayList.js
@@ -1,25 +1,64 @@
-import dummy from "../db/data.json"
+// import dummy from "../db/data.json" : REST API, json-server 설치 시 필요없음
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 export default function DayList(){
 
-    console.log(dummy);
+    const [days, setDays] = useState([]); // 처음에는 빈 배열로 만들고 API에서 리스트를 가져와 바꾸는 방식
+                                          // 데이터가 바뀌면 자동 렌더링
 
+    useEffect(() => {
+        fetch("http://localhost:3001/days")
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setDays(data);
+        })
+    }, []);
+
+
+    // const [count, setCount] = useState(0);
+    // const setCountfunc = () => {
+    //     setCount(count+1);
+    // }
+
+    // useEffect(() => { // 이 함수의 호출시점은 렌더링 후 DOM에 반영된 직후
+    //                 // 상태값이 변경되고 렌더링 후 호출됨
+    //                 // 
+    //     console.log("Count changed to " + count);
+    // }, []);      // 빈 배열을 넘기면 최초의 1번만 호출된다.
+    //              // 두 번째 매개 변수로 [count]를 넘기면 count가 변경될 때만 useEffect가 호출된다.
+    //              // 의존성 배열이라 한다.
+
+    
+    // // let a = [...b] : b의 배열이 a에 복사된다.
+    // const setDaysfunc = () => {
+    //     setDays( [
+    //         ...days,
+    //         {
+    //             id : Math.random(),
+    //             day : 1
+    //         }
+    //         ]
+            
+    //     );
+    // }
 
     // map() : 배열의 요소를 돌면서 인자로 전달된 함수를 사용하여
     // 처리된 새로운 결과를 새로운 배열에 담아 반환하는 함수
-
-
-
-
-
     // 리액트는 a href대신 Link to를 사용한다.
     return (
+        
         <ul className="list_day">
-            {dummy.days.map(day => (
+            {days.map(day => (
                 <li key ={day.id}> 
                 <Link to={`/day/${day.day}`}> Day {day.day}</Link></li>
                 ))}
         </ul>
+        // <button onClick={setCountfunc}>{count}</button>
+        // <button onClick={setDaysfunc}>Day Change</button> 
+        
     );
 }
 
--- react-foundation-youtube/src/index.js
+++ react-foundation-youtube/src/index.js
@@ -9,7 +9,17 @@ import "./index.css";
 // 모든 js 파일은 모듈이다.
 // Part1 : #1 ~ #9
 
+
 // npm install react-router-dom : 라우터 설치
+
+
+// npm install -g json-server
+// json-server --watch ./src/db/data.json --port 3001
+// Rest API : url 주소와 method로 CRUD 요청
+// C : POST
+// R : GET
+// U : PUT
+// D : DELETE
 
 ReactDOM.render(
   <div>
