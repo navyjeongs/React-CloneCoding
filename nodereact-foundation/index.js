14963dd3f65618cb0892fe9b2095df8f51be8eb1
Jeong Seok <wjdtjr8649@naver.com>
2022-03-26 21:08:12 +0900

    03-26 Login 구현중

--- nodereact-foundation/index.js
+++ nodereact-foundation/index.js
@@ -9,8 +9,13 @@ const { User } = require("./models/User");
 
 const mongoose = require("mongoose");
 
+const cookieParser = require("cookie-parser");
+
 // connect의 AppCode를 config/key에서 가져오기
 const config = require("./config/key");
+
+app.use(cookieParser());
+
 // Connect 성공시 then 실행, 실패시 catch 출력
 mongoose
   .connect(config.mongoURI)
@@ -18,7 +23,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello get Request");
+  res.send("Hello get Request!");
 });
 
 //application/x-www-form-urlencoded 으로 된 data를 분석해서 가져옴
@@ -29,6 +34,7 @@ app.use(express.json());
 app.post("/register", (req, res) => {
   // 회원가입시 작성한 정보를 client에서 가져오면 그것을 DB에 넣어준다.
   const user = new User(req.body);
+
   user.save((err, userInfo) => {
     if (err) {
       return res.json({ success: false, err });
@@ -36,6 +42,39 @@ app.post("/register", (req, res) => {
     return res.status(200).json({
       success: true,
     });
+  });
+});
+
+// 로그인
+app.post("/login", (res, req) => {
+  // 1. 요청된 email이 DB에 있는지 확인
+  User.findOne({ email: req.body.email }, (err, userInfo) => {
+    // email이 없다면
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "Email에 해당하는 User가 존재하지 않습니다.",
+      });
+    }
+  });
+  // 2. email이 있다면 password가 맞는지 확인
+  userInfo.comparePassword(req.body.password, (err, isMatch) => {
+    if (!isMatch) {
+      return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 일치하지 않습니다.",
+      });
+    }
+  });
+  // 3. password가 맞다면 token 생성
+  user.generateToken((err, user) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    res
+      .cookies("x_auth", user.token)
+      .status(200)
+      .json({ loginSuccess: true, userId: user._id });
   });
 });
 
--- nodereact-foundation/models/User.js
+++ nodereact-foundation/models/User.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+// https://www.npmjs.com/package/bcrypt : bcrypt 사용법
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const jwt = require("jsonwebtoken");
 // 스키마 작성
 const userSchema = mongoose.Schema({
   name: {
@@ -32,6 +37,58 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+// userSchema를 mongoose에 save하기 전에 function을 실행한다.
+// function을 실행후 index.js의 user.save로 이동한다.
+userSchema.pre("save", function (next) {
+  var user = this;
+  // password가 변경될 때만 비밀번호 암호화작업을 하게
+  if (user.isModified("password")) {
+    // password 암호화 작업
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      // myPlaintextPassword : Client가 입력한 password
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } // password를 제외한 다른 것을 변경할 때 index.js의 user.save로 이동
+  else {
+    next();
+  }
+});
+
+// 로그인시
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  // plainpassword를 암호화된 비밀번호와 일치하는지 확인
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+    // err이 없고 같을 때
+    callback(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (callback) {
+  var user = this;
+  // jsonwebtoken을 이용해 token 생성
+  var token = jwt.sign(user._id.toHexString(), "secrettokennn");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) {
+      return callback(err);
+    }
+    // 에러가 없을 때
+    callback(null, user);
+  });
+};
 
 // Schema를 Model로 감싼다.
 const User = mongoose.model("User", userSchema);
