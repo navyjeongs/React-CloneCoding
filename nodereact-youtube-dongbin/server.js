58af9f654bb971b96f3e2a65a23f1581ec140fff
Jeong Seok <wjdtjr8649@naver.com>
2022-03-26 22:42:37 +0900

    03.26 08. Client API

--- nodereact-youtube-dongbin/server.js
+++ nodereact-youtube-dongbin/server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello!!" });
+// 고객 정보 받아오는 API 만들기
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      name: "navy",
+      birthday: "000101",
+      gender: "남자",
+      job: "대학생",
+      image: "https://placeimg.com/100/100/1",
+    },
+    {
+      id: 2,
+      name: "army",
+      birthday: "010101",
+      gender: "남자",
+      job: "대학원",
+      image: "https://placeimg.com/100/100/12",
+    },
+    {
+      id: 3,
+      name: "air",
+      birthday: "020202",
+      gender: "여자",
+      job: "고등학생",
+      image: "https://placeimg.com/100/100/3",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
