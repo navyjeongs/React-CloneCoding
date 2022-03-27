92571fb4c4a72b9e5c851d9500231a04aff2f2f7
Jeong Seok <wjdtjr8649@naver.com>
2022-03-27 17:52:16 +0900

    03.24 11. connect mysql and get data from mysql database

--- nodereact-youtube-dongbin/server.js
+++ nodereact-youtube-dongbin/server.js
@@ -7,37 +7,30 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// express 4.16 이상에서는 필요없다.
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// 고객 정보 받아오는 API 만들기
+const fs = require("fs"); // File System Module을 가져온다.
+const data = fs.readFileSync("./database.json"); // 동기 처리를 통해 파일을 읽은 후 실행한다.
+const conf = JSON.parse(data); // json 문자열을 javascripte 객체로 생성
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  //mysql 연결을 위한 코드
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+
+// 고객 정보 받아오는 API
+// client로 부터 /api/customers 주소로 요청이 오면 customer의 모든 data를 보내준다.
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "navy",
-      birthday: "000101",
-      gender: "남자",
-      job: "대학생",
-      image: "https://placeimg.com/100/100/1",
-    },
-    {
-      id: 2,
-      name: "army",
-      birthday: "010101",
-      gender: "남자",
-      job: "대학원",
-      image: "https://placeimg.com/100/100/12",
-    },
-    {
-      id: 3,
-      name: "air",
-      birthday: "020202",
-      gender: "여자",
-      job: "고등학생",
-      image: "https://placeimg.com/100/100/3",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.get("/", (req, res) => {
