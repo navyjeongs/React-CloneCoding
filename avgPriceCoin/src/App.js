89c2ffe06a183060de21bdb72acd013a8d1f2499
Jeong Seok <wjdtjr8649@naver.com>
2022-04-09 02:33:28 +0900

    04-09 Update Routes

--- avgPriceCoin/src/App.js
+++ avgPriceCoin/src/App.js
@@ -1,24 +1,10 @@
-import Input from "./getCoin/input";
-import Header from "./header/Header";
-import CoinList from "./coinList/CoinList";
-import { BrowserRouter, Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
-import UpbitList from "./UpbitList";
-import AvgPriceCoin from "./avgPriceCoin/AvgPriceCoin";
+import AppRouter from "./routers/AppRouter";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-
-        <Routes>
-          <Route path="/" element={<AvgPriceCoin />} />
-          <Route path="/CoinList" element={<CoinList />} />
-          <Route path="/GetCoin" element={<Input />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div>
+      <AppRouter />
+    </div>
   );
 }
 
